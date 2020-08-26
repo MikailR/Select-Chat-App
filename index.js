68b6d2a75d7ca62672cb9f44273b046193b91fdb
Rajeev Parmasar <rajeevparmasar@Rajs-Air.hitronhub.home>
2020-08-26 18:01:05 -0400

    Updates

--- index.js
+++ index.js
@@ -61,7 +61,7 @@ app.post('/token', (request, response) => {
 
 if(process.env.NODE_ENV === 'production'){
   app.use(express.static('./build'));
-  app.get('*', (req, res) => {
+  app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'build', 'index.js'));
   })
 }
