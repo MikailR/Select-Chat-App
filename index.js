8973f736ab7039f36ea22059a6bc5cbc9d1ab995
Rajeev Parmasar <rajeevparmasar@Rajs-Air.hitronhub.home>
2020-08-27 15:55:59 -0400

    changed homepage in package.json

--- index.js
+++ index.js
@@ -87,7 +87,7 @@ if(process.env.NODE_ENV === 'production'){
 // app.listen(config.port, () => {
 //   console.log(`Application started at localhost:${config.port}`);
 // });
-app.listen(process.env.PORT || 3001, function(){
+app.listen(process.env.PORT || 3000, function(){
   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
 
